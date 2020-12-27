518cc1296536595a0c0bc6ff6ef78ab9842b6a4e
njahan98 <njahan3383@bths.edu>
2020-12-27 15:08:55 -0500

    extent reports

--- generic/src/main/java/com/base/TestBase.java
+++ generic/src/main/java/com/base/TestBase.java
@@ -1,22 +1,39 @@
 package com.base;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
+import com.report.ExtentManager;
+import com.report.ExtentTestManager;
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     public static WebDriver driver;
+    public static ExtentReports extent;
     public static String sauceUserName = "nusrutjahan1";
     public static String sauceKey = "ff3b5dce-9b66-4333-b00b-ca98460d9b85";
     public static String browserStackUserName = "nusrutjahan1";
@@ -111,6 +128,83 @@ public class TestBase {
 
     public static void clickOnElement(String element) {
         driver.findElement(By.xpath(element)).click();
+    }
+
+    //screenshot used to capture failures
+    public static void captureScreenshot(WebDriver driver, String screenshotName) {
+        DateFormat df = new SimpleDateFormat("HH_mm_ss");
+        Date date = new Date();
+        df.format(date);
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File(System.getProperty("user.dir") + "/screenshots/" + screenshotName + " " + df.format(date) + ".jpg"));
+            System.out.println("Screenshot captured");
+        } catch (Exception e) {
+            System.out.println("Exception while taking screenshot " + e.getMessage());
+        }
+    }
+
+    //reporting starts created by extent report guys in order to use in framework
+    @BeforeSuite
+    public void extentSetup(ITestContext context) {
+        ExtentManager.setOutputDirectory(context);
+        extent = ExtentManager.getInstance();
+    }
+
+    @BeforeMethod
+    public void startExtent(Method method) {
+        String className = method.getDeclaringClass().getSimpleName();
+        ExtentTestManager.startTest(method.getName());
+        ExtentTestManager.getTest().assignCategory(className);
+    }
+
+    protected String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        t.printStackTrace(pw);
+        return sw.toString();
+    }
+
+    @AfterMethod
+    public void afterEachTestMethod(ITestResult result) {
+        ExtentTestManager.getTest().getTest().setStartedTime(getTime(result.getStartMillis()));
+        ExtentTestManager.getTest().getTest().setEndedTime(getTime(result.getEndMillis()));
+        for (String group : result.getMethod().getGroups()) {
+            ExtentTestManager.getTest().assignCategory(group);
+        }
+
+        if (result.getStatus() == 1) {
+            ExtentTestManager.getTest().log(LogStatus.PASS, "Test Passed");
+        } else if (result.getStatus() == 2) {
+            ExtentTestManager.getTest().log(LogStatus.FAIL, getStackTrace(result.getThrowable()));
+        } else if (result.getStatus() == 3) {
+            ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+        }
+
+        ExtentTestManager.endTest();
+        extent.flush();
+        if (result.getStatus() == ITestResult.FAILURE) {
+            captureScreenshot(driver, result.getName());
+        }
+    }
+
+    public Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();
+    }
+
+    @AfterSuite
+    public void generateReport() {
+        extent.close();
+    }
+    //reporting finish to automatically generate report
+
+    @AfterMethod //once test cases get executed, we want to close our browser so it always closes it
+    public void cleanUp() {
+        driver.close();
+        driver.quit();
+        LOGGER.info("driver closed");
     }
 
 }
--- nordstrom/src/main/java/com/nordstrom/pages/HomePage.java
+++ nordstrom/src/main/java/com/nordstrom/pages/HomePage.java
@@ -4,27 +4,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
+
     @FindBy(xpath = "(//span[@class='_3a22W'])")
     private WebElement searchButton;
-
-    @FindBy(xpath = "(//button[@class='_31pVq _1l-nn _3CU6a _1sMDh _2PDR1'])")
-    private WebElement saleButton;
-
-    @FindBy(xpath = "(//button[@class='_31pVq UfYO5 _3CU6a _1sMDh _2PDR1'])")
-    private WebElement womensDropDown;
-
 
     public void clickSearchButton() {
         searchButton.click();
     }
-
     public void typeOnSearchBar() {
         searchButton.sendKeys("handbags");
     }
 
+    @FindBy(xpath = "(//button[@class='_31pVq _1l-nn _3CU6a _1sMDh _2PDR1'])")
+    private WebElement saleButton;
+
     public void clickSaleButton() {
         saleButton.click();
     }
+
+    @FindBy(xpath = "(//button[@class='_31pVq UfYO5 _3CU6a _1sMDh _2PDR1'])")
+    private WebElement womensDropDown;
 
     public void clickWomensDropDownButton() {
         womensDropDown.click();
