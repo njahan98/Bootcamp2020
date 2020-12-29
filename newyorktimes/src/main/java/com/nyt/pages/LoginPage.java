99a78b0006625956b1b8e6f9e5b290df5ac75ee8
njahan98 <njahan3383@bths.edu>
2020-12-28 20:13:07 -0500

    ny times locked me out woops

--- newyorktimes/src/main/java/com/nyt/pages/LoginPage.java
+++ newyorktimes/src/main/java/com/nyt/pages/LoginPage.java
@@ -1,6 +1,34 @@
 package com.nyt.pages;
 
 import com.base.TestBase;
+import com.report.ExtentTestManager;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends TestBase {
+
+    @FindBy(linkText = "Log in")
+    private WebElement loginButton;
+
+    @FindBy(id = "username")
+    private WebElement emailField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    public void clickLoginButton(){
+        loginButton.click();
+        ExtentTestManager.log("Login button is pressed");
+    }
+    public void enterCredentials(){
+        emailField.click();
+        emailField.sendKeys("tursunnahaj@gmail.com");
+        ExtentTestManager.log("Email is entered into field");
+        sleepFor(5);
+        passwordField.click();
+        passwordField.sendKeys("TestingPage123!",Keys.ENTER);
+        sleepFor(5);
+        ExtentTestManager.log("Passsword is entered into field and user logs into account");
+    }//ny times currently locked out my email address
 }
--- newyorktimes/src/test/java/com/nyttest/LoginPageValidations.java
+++ newyorktimes/src/test/java/com/nyttest/LoginPageValidations.java
@@ -5,6 +5,7 @@ import com.nyt.pages.LoginPage;
 import com.nyt.pages.MainPage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import sun.jvm.hotspot.debugger.Page;
 
 public class LoginPageValidations extends TestBase {
@@ -13,6 +14,18 @@ public class LoginPageValidations extends TestBase {
     @BeforeMethod
     public void pageFactorySetup() {
         mainPage = PageFactory.initElements(driver, MainPage.class);
+        mainPage.nytMainPage();
         loginPage = PageFactory.initElements(driver,LoginPage.class);
+    }
+    @Test(enabled = false)
+    public void validateLoginButton() {
+        sleepFor(5);
+        loginPage.clickLoginButton();
+        sleepFor(5);
+    }
+    @Test
+    public void validateCredentials() {
+        validateLoginButton();
+        loginPage.enterCredentials();
     }
 }
